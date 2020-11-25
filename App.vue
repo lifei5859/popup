a77fde7636ce8c90511d1000eca4edfade5347cc
mac <mac@macdeiMac.local>
2020-11-25 08:53:56 +0800

    init

---
+++ App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		onLaunch: function() {
+			
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/* #ifndef APP-NVUE */
+	@import "./static/iconfont.css";
+	/* #endif */
+	/* #ifdef APP-NVUE */
+	.iconfont {
+		font-family: iconfont;
+		font-size: 50upx;
+		color: #999999;
+	}
+	
+	/* #endif */
+</style>
